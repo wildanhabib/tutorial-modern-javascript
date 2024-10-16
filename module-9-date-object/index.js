b78ca0ae00da3be75b0b78f3b074bc1ccfa0f354
wildanhabib <wildanhabib051@gmail.com>
2024-10-16 12:56:03 +0700

    rubah date

--- module-9-date-object/index.js
+++ module-9-date-object/index.js
@@ -11,7 +11,7 @@ let now = new Date()
 
 // Menggunakan string
 let specificDate = new Date("August 20, 2024 10:30:00")
-// console.log(specificDate)
+console.log(specificDate)
 
 
 // Menggunakan parameter (tahun, bulan, hari, jam, menit, detik, milidetik)
